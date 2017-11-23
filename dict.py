6c9fd6030c88c602edb9b83eb4cc3e437969bc8d
Daniel Huang <gn03249822@DanieltekiMacBook-Pro.local>
2017-11-23 01:32:50 -0800

    user prompt implemented

--- dict.py
+++ dict.py
@@ -13,9 +13,16 @@ def translate(word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean %s instead?" %get_close_matches(word,data.keys())[0]   #return the most similar match in the matches list
+        userAns = input("Did you mean %s instead? Enter Y if yes, or N if no: " %get_close_matches(word,data.keys())[0])
+        if userAns == 'Y' or 'y':
+            # return the most similar match in the matches list
+            return data[get_close_matches(word,data.keys())[0]]
+        elif userAns == 'N' or 'n':
+            return "The word doesn't exist. Please double check again."
+        else:
+            return "Sorry, we didn't understand your entry."
     else:
-        return "The word doesn't exist. Please double check again"
+        return "The word doesn't exist. Please double check again."
 
 word = input("Enter the word you want to look up for: ")
 print(translate(word))
